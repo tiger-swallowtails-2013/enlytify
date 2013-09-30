887e0c81625d33666cad43bce0615c073a73e56f
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-09-30 15:48:59 -0700

    adding config to gitignore

--- app.rb
+++ app.rb
@@ -1,17 +1,12 @@
-# require 'sinatra'
-# require 'sinatra/activerecord'
-
-# ENV['RACK_ENV'] == 'test' ? DB_NAME = "enlytifydb_test" : DB_NAME = "enlytifydb"
-# ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "postgresql://localhost/#{DB_NAME}")
-
-
-# get '/' do
-#   "Hello world"
-# end
-
+require 'sinatra'
+require 'sinatra/activerecord'
 require 'dbc-ruby'
 require_relative 'config'
 
-DBC.token = TOKEN
+ENV['RACK_ENV'] == 'test' ? DB_NAME = "enlytifydb_test" : DB_NAME = "enlytifydb"
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "postgresql://localhost/#{DB_NAME}")
 
-p DBC::User.find(25)
+
+get '/' do
+  "Hello world"
+end

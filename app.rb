7c64220696a4b9deb7954934f77f51941496608c
Lionel Ramos <lionelramos@Lionels-MacBook-Pro.local>
2013-09-30 15:21:03 -0700

    add require sinatra/activerecord to app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 
 ENV['RACK_ENV'] == 'test' ? DB_NAME = "enlytifydb_test" : DB_NAME = "enlytifydb"
 ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "postgresql://localhost/#{DB_NAME}")

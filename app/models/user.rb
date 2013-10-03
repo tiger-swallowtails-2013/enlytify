7e4c07f72a4a0db8f3944d9e30c21ab33872b27b
Apprentice <apprentice@dbc15.local>
2013-10-03 15:41:31 -0700

    DEBUGGINGgit add -p! tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@ class User
   attr_reader :fullname, :gravatar, :email, :github_profile, :cohort_id, :dbc_student_id
   
   def initialize(user_attributes)
+    user_attributes = HashWithIndifferentAccess.new(user_attributes)
     @fullname = user_attributes['name']
     @gravatar = user_attributes['gravatar']
     @email = user_attributes['email']
-    @github_profile = DBC::User.find(user_attributes['id']).profile[:github]
-    @cohort_id = DBC::User.find(user_attributes['id']).cohort_id
+    @github_profile = find(user_attributes['id']).profile[:github]
+    @cohort_id = p find(user_attributes['id']).cohort_id
     @dbc_student_id = user_attributes['id']
   end 
 
   def find(id)
+    raise "WAT" if id.nil?
     DBC::User.find(id)
   end 
 end 
--- spec/talk_spec.rb
+++ spec/talk_spec.rb
@@ -36,16 +36,21 @@ end
 feature 'the current user is not the author of the talk', :type => :feature do
 
   before :each do
-    @current_user = User.new({'name' => 'Morgan Wildermuth', 'email' => "morgan.wildermuth@gmail.com", 'gravatar' => "https://secure.gravatar.com/avatar/c08bab8ea8340a2598b06a54beade3cb.png?d=mm&r=PG&s=250", 'id' => 633})
+    @talk = Talk.create
+    @talk.id = 1
+    @talk.speaker_id = 1
+    @talk.save
+    # @current_user = User.new(session[{fullname: 'Morgan Wildermuth', email: "morgan.wildermuth@gmail.com", gravatar: "https://secure.gravatar.com/avatar/c08bab8ea8340a2598b06a54beade3cb.png?d=mm&r=PG&s=250", id: 633, cohort_id: 29}])
+    login_as({fullname: 'Morgan Wildermuth', dbc_student_id: 633})
     visit("/talk/#{@talk.id}")
   end
 
   scenario 'does not show the description form' do
-    page.should have_selector('#description', visible: false)
+    expect(page).to have_no_selector('#desc_form')
   end
 
   scenario 'does not show the resource form' do
-    page.should have_selector('#resource_form', visible: false)
+    page.should have_no_selector('#resources_form')
   end
 end
 
